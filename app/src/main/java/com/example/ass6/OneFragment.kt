8da3ac033ff5e8e6a7b5f0e64e053139c45cb3a3
terntien <54529239+terntien@users.noreply.github.com>
2019-09-25 21:09:10 +0700

    ASS6_SEC1_603020775-5

---
+++ app/src/main/java/com/example/ass6/OneFragment.kt
@@ -0,0 +1,27 @@
+package com.example.ass6
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import com.example.ass6.R
+import kotlinx.android.synthetic.main.fragment_one.view.*
+import kotlinx.android.synthetic.main.fragment_two.view.*
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class OneFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_one, container, false)
+        return view
+    }
+
+}
